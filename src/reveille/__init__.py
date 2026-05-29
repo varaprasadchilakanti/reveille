06f5d3eb1c43b3942f761d9cf73cf1259a7e204a
Varaprasad Chilakanti <varaprasadchilakanti@gmail.com>
2026-05-29 15:11:14 +0530

    chore(release): bump version to 0.6.0

--- src/reveille/__init__.py
+++ src/reveille/__init__.py
@@ -4,7 +4,7 @@ A CLI tool that generates self-contained HTML performance reports
 from local Git repositories.
 """
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __author__ = "Varaprasad Chilakanti"
 __email__ = "varaprasadchilakanti@gmail.com"
 __licence__ = "MIT"
